05f31332e4176b3a6bf01d2927c49c711575d211
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2024-10-10 22:50:45 +0700

    Update

--- backend/src/resume-builders/resume-builders.service.ts
+++ backend/src/resume-builders/resume-builders.service.ts
@@ -5,17 +5,51 @@ import { ResumeBuilder, ResumeBuilderDocument } from './schemas/resume-builder.s
 import { InjectModel } from '@nestjs/mongoose';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import mongoose from 'mongoose';
+import { IUser } from 'src/users/users.interface';
 
 @Injectable()
 export class ResumeBuildersService {
   constructor(@InjectModel(ResumeBuilder.name) private resumeBuidlerModel: SoftDeleteModel<ResumeBuilderDocument>) { }
 
-  async create(createResumeBuilderDto) {
+  async create(createResumeBuilderDto, user: IUser) {
     let newResumeBuilder = await this.resumeBuidlerModel.create({
-      ...createResumeBuilderDto
-    })
+      ...createResumeBuilderDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
+    });
+    return newResumeBuilder;
+  }
 
-    return newResumeBuilder
+  async update(_id: string, updateResumeBuilderDto, user: IUser) {
+    const updated = await this.resumeBuidlerModel.updateOne(
+      { _id },
+      {
+        ...updateResumeBuilderDto,
+        updatedBy: {
+          _id: user._id,
+          email: user.email
+        }
+      }
+    );
+    return updated;
+  }
+
+  async remove(id: string, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return `Not found resumeBuilder`;
+    }
+    await this.resumeBuidlerModel.updateOne(
+      { _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email
+        }
+      }
+    );
+    return this.resumeBuidlerModel.softDelete({ _id: id });
   }
 
   findByUserId(userId: string) {
@@ -34,21 +68,4 @@ export class ResumeBuildersService {
     return this.resumeBuidlerModel.findById(id)
   }
 
-  async update(_id: string, updateJobDto) {
-    const updated = await this.resumeBuidlerModel.updateOne(
-      { _id },
-      {
-        ...updateJobDto
-      })
-    return updated
-  }
-
-  async remove(id: string) {
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return `Not found resumeBuilder`
-    }
-    return this.resumeBuidlerModel.softDelete({
-      _id: id,
-    })
-  }
 }
