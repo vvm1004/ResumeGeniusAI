b192d2c31ad9ec20bb75bdb0db9294b598fd888e
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2024-10-19 18:02:53 +0700

    Update template api

--- backend/src/template/template.controller.ts
+++ backend/src/template/template.controller.ts
@@ -1,29 +1,51 @@
-import { Controller, Get, Post, Patch, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Body, Param, Delete } from '@nestjs/common';
 import { TemplateService } from './template.service';
-import { Public } from 'src/decorator/customize';
+import { Public, ResponseMessage, SkipCheckPermission, User } from 'src/decorator/customize';
 import { ApiTags } from '@nestjs/swagger';
+import { IUser } from 'src/users/users.interface';
 
 @ApiTags('template')
 @Controller('template')
 export class TemplateController {
     constructor(private readonly templateService: TemplateService) { }
 
-    @Public()
-    @Get()
-    findAll() {
-        
-        return this.templateService.findAll();
-    }
-    @Public()
-    @Post()
-    create(@Body() templateData: any) {
-        // Logic to create a new template
-        return this.templateService.create(templateData);
-    }
-    @Public()
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateData: any) {
-        // Logic to update a template based on id
-        return this.templateService.update(id, updateData);
-    }
+    // @Public()
+  @SkipCheckPermission()
+  @ResponseMessage("Create a new template")
+  @Post()
+  create(@Body() createTemplateDto, @User() user: IUser) {
+    return this.templateService.create(createTemplateDto, user);
+  }
+
+  @Public()
+  @SkipCheckPermission()
+  @ResponseMessage("Fetch All Template")
+  @Get()
+  findAll() {
+    return this.templateService.findAll();
+  }
+
+  // @Public()
+  @SkipCheckPermission()
+  @ResponseMessage("Fetch Template by id")
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.templateService.findOne(id);
+  }
+
+  // @Public()
+  @SkipCheckPermission()
+  @ResponseMessage("Update a template")
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTemplateDto, @User() user: IUser) {
+    return this.templateService.update(id, updateTemplateDto, user);
+  }
+
+  // @Public()
+  @SkipCheckPermission()
+  @ResponseMessage("Delete a template")
+  @Delete(':id')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.templateService.remove(id, user);
+  }
 }
--- backend/src/template/template.service.ts
+++ backend/src/template/template.service.ts
@@ -1,29 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Template, TemplateDocument } from './schemas/template.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/users.interface';
 
 @Injectable()
 export class TemplateService {
-    constructor(@InjectModel(Template.name) private templateModel: Model<TemplateDocument>) {}
+    constructor(@InjectModel(Template.name) private templateModel: SoftDeleteModel<TemplateDocument>) { }
 
-    getHello(): string {
-        return 'Hello from TemplateService!';
-    }
-
-    async findAll(): Promise<Template[]> {
+    async create(createTemplateDtoo, user: IUser) {
+        let newTemplate = await this.templateModel.create({
+          ...createTemplateDtoo,
+          createdBy: {
+            _id: user._id,
+            email: user.email
+          }
+        });
+        return newTemplate;
+      }
+    
+      async update(_id: string, updateTemplateDtoo, user: IUser) {
+        const updated = await this.templateModel.updateOne(
+          { _id },
+          {
+            ...updateTemplateDtoo,
+            updatedBy: {
+              _id: user._id,
+              email: user.email
+            }
+          }
+        );
+        return updated;
+      }
+    
+      async remove(id: string, user: IUser) {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+          return `Not found resumeBuilder`;
+        }
+        await this.templateModel.updateOne(
+          { _id: id },
+          {
+            deletedBy: {
+              _id: user._id,
+              email: user.email
+            }
+          }
+        );
+        return this.templateModel.softDelete({ _id: id });
+      }
+    
+    
+      findAll() {
         return this.templateModel.find().exec();
-    }
-
-    async create(templateData: any): Promise<Template> {
-        const createdTemplate = new this.templateModel(templateData);
-        return createdTemplate.save();
-    }
-
-    async update(id: string, updateData: any): Promise<Template> {
-        return this.templateModel.findByIdAndUpdate(id, updateData, { new: true }).exec();
-    }
-
-    // Add more methods as needed
+    
+      }
+    
+      async findOne(id: string) {
+        if (!mongoose.Types.ObjectId.isValid(id))
+          throw new BadRequestException("Not found template with id: " + id)
+        return this.templateModel.findById(id)
+      }
+    
 }
 
