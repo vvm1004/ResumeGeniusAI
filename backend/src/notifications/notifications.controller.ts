c7d7f0a7eaf8846cf9bbdd12625c382c4198fd53
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2024-11-30 20:37:16 +0700

    Chi doi ten thu muc

--- backend/src/notifications/notifications.controller.ts
+++ backend/src/notifications/notifications.controller.ts
@@ -14,6 +14,7 @@ export class NotificationsController {
     findAll() {
         return this.notificationsService.findAll();
     }
+    @Public()
     @Post()
     create(@Body() createNotificationDto: CreateNotificationDto) {
         return this.notificationsService.create(createNotificationDto);
